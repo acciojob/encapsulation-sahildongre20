d3bb0e2453cb9ad1a76768fac9ec1c0d8adecff8
Sahil Dongre <sahildongre1000@gmail.com>
2023-08-15 10:21:44 +0530

    Inital Commmit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        //java: name has private access in com.driver.RWOnly
+       // rwOnly.name="Abc";
+
+        rwOnly.setName("Abc");
+        rwOnly.getName();
+
+
+
+    }
 }
